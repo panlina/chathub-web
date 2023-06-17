5e84280de89983ce6b634080b2ad04485e079c49
Jack Lu <jacklu@jacklu.me>
2023-06-17 22:42:18 +0800

    extract Table

--- src/App.js
+++ src/App.js
@@ -1,115 +1,40 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import { useState } from 'react';
+import Table from './Table';
 function App() {
   var queryClient = useQueryClient();
-  var app = useQuery(['app'], async () => (await axios.get("/app")).data);
-  var [newApp, setNewApp] = useState({ name: '', value: '' });
-  var addApp = useMutation({
-    mutationFn: () => axios.put(`/app/${newApp.name}`, JSON.stringify(newApp.value), { headers: { 'Content-Type': 'application/json' } }),
-    onSuccess: () => { queryClient.setQueryData(['app'], old => (old[newApp.name] = newApp.value, old)) }
-  });
-  var deleteApp = useMutation({
-    mutationFn: name => axios.delete(`/app/${name}`),
-    onSuccess: (data, name) => { queryClient.setQueryData(['app'], old => (delete old[name], old)) }
-  });
-  var [editedApp, setEditedApp] = useState();
-  var updateApp = useMutation({
-    mutationFn: () => axios.put(`/app/${editedApp.name}`, JSON.stringify(editedApp.value), { headers: { 'Content-Type': 'application/json' } }),
-    onSuccess: () => { queryClient.setQueryData(['app'], old => (old[editedApp.name] = editedApp.value, old)) }
-  });
-  var [renamedApp, setRenamedApp] = useState();
-  var renameApp = useMutation({
-    mutationFn: () => axios(`/app/${renamedApp.name}`, { method: 'MOVE', headers: { 'Content-Type': 'application/json', 'Destination': `/app/${encodeURIComponent(renamedApp.newName)}`, 'Overwrite': 'F' } }),
-    onSuccess: () => { queryClient.setQueryData(['app'], old => { var oldValue = old[renamedApp.name]; delete old[renamedApp.name]; old[renamedApp.newName] = oldValue; return old; }); }
-  });
   return <>
     <h1>Chathub</h1>
     <section>
       <h2>app</h2>
-      <table>
-        {app.isLoading ? <tr><td colSpan={3}>(Loading...)</td></tr> :
-          Object.keys(app.data).length ?
-            Object.entries(app.data).map(([name, value]) =>
-              <tr key={name}>
-                <td>{
-                  renamedApp && renamedApp.name == name ?
-                    <input form="rename-app" value={renamedApp.newName} onChange={e => { setRenamedApp({ ...renamedApp, newName: e.target.value }); }} /> :
-                    name
-                }</td>
-                <td>{
-                  editedApp && editedApp.name == name ?
-                    <input form="update-app" value={editedApp.value} onChange={e => { setEditedApp({ ...editedApp, value: e.target.value }); }} /> :
-                    <code>{value}</code>
-                }</td>
-                <td>
-                  <button
-                    onClick={async () => {
-                      try { await deleteApp.mutateAsync(name); }
-                      catch (e) { alert(e.response.data); }
-                    }}
-                    disabled={addApp.isLoading || deleteApp.isLoading || editedApp || renamedApp}
-                  >
-                    {deleteApp.isLoading && deleteApp.variables == name ? "Deleting..." : "❌"}
-                  </button>
-                  {
-                    editedApp && editedApp.name == name && !updateApp.isLoading ?
-                      <>
-                        <button form="update-app">OK</button>
-                        <button form="update-app" onClick={() => { setEditedApp(undefined); }}>Cancel</button>
-                      </> :
-                      <button onClick={() => { setEditedApp({ name: name, value: value }); }} disabled={addApp.isLoading || deleteApp.isLoading || editedApp || renamedApp}>{
-                        updateApp.isLoading && editedApp.name == name ? "Updating..." : "🖊"
-                      }</button>
-                  }
-                  {
-                    renamedApp && renamedApp.name == name && !renameApp.isLoading ?
-                      <>
-                        <button form="rename-app">OK</button>
-                        <button form="rename-app" onClick={() => { setRenamedApp(undefined); }}>Cancel</button>
-                      </> :
-                      <button onClick={() => { setRenamedApp({ name: name, newName: name }); }} disabled={addApp.isLoading || deleteApp.isLoading || editedApp || renamedApp}>{
-                        renameApp.isLoading && renamedApp.name == name ? "Renaming..." : "Rename"
-                      }</button>
-                  }
-                </td>
-              </tr>
-            ) :
-            <tr><td colSpan={3}>(No apps)</td></tr>
+      <Table
+        get={useQuery(['app'], async () => (await axios.get("/app")).data)}
+        add={
+          useMutation({
+            mutationFn: newApp => axios.put(`/app/${newApp.name}`, JSON.stringify(newApp.value), { headers: { 'Content-Type': 'application/json' } }),
+            onSuccess: (data, newApp) => { queryClient.setQueryData(['app'], old => (old[newApp.name] = newApp.value, old)) }
+          })
         }
-        <tr>
-          <td><input form="add-app" value={newApp.name} onChange={e => { setNewApp({ ...newApp, name: e.target.value }); }} /></td>
-          <td><input form="add-app" value={newApp.value} onChange={e => { setNewApp({ ...newApp, value: e.target.value }); }} /></td>
-          <td><button form="add-app" disabled={addApp.isLoading || deleteApp.isLoading || editedApp || renamedApp}>{addApp.isLoading ? "Adding..." : "➕"}</button></td>
-        </tr>
-        <form id="add-app" onSubmit={async e => {
-          e.preventDefault();
-          try {
-            await addApp.mutateAsync();
-            setNewApp({ name: '', value: '' });
-          } catch (e) {
-            alert(e.response.data);
-          }
-        }} />
-        <form id="update-app" onSubmit={async e => {
-          e.preventDefault();
-          try {
-            await updateApp.mutateAsync();
-            setEditedApp(undefined);
-          } catch (e) {
-            alert(e.response.data);
-          }
-        }} />
-        <form id="rename-app" onSubmit={async e => {
-          e.preventDefault();
-          try {
-            await renameApp.mutateAsync();
-            setRenamedApp(undefined);
-          } catch (e) {
-            alert(e.response.data);
-          }
-        }} />
-      </table>
+        _delete={
+          useMutation({
+            mutationFn: name => axios.delete(`/app/${name}`),
+            onSuccess: (data, name) => { queryClient.setQueryData(['app'], old => (delete old[name], old)) }
+          })
+        }
+        update={
+          useMutation({
+            mutationFn: editedApp => axios.put(`/app/${editedApp.name}`, JSON.stringify(editedApp.value), { headers: { 'Content-Type': 'application/json' } }),
+            onSuccess: (data, editedApp) => { queryClient.setQueryData(['app'], old => (old[editedApp.name] = editedApp.value, old)) }
+          })
+        }
+        rename={
+          useMutation({
+            mutationFn: renamedApp => axios(`/app/${renamedApp.name}`, { method: 'MOVE', headers: { 'Content-Type': 'application/json', 'Destination': `/app/${encodeURIComponent(renamedApp.newName)}`, 'Overwrite': 'F' } }),
+            onSuccess: (data, renamedApp) => { queryClient.setQueryData(['app'], old => { var oldValue = old[renamedApp.name]; delete old[renamedApp.name]; old[renamedApp.newName] = oldValue; return old; }); }
+          })
+        }
+        renderValue={value => <code>{value}</code>}
+      />
     </section>
   </>;
 }
---
+++ src/Table.js
@@ -0,0 +1,95 @@
+import { useState, useId } from 'react';
+export default function Table({
+  get, add, _delete, update, rename,
+  renderValue
+}) {
+  var [_new, setNew] = useState({ name: '', value: '' });
+  var addFormId = useId();
+  var [edited, setEdited] = useState();
+  var updateFormId = useId();
+  var [renamed, setRenamed] = useState();
+  var renameFormId = useId();
+  return <table>
+    {get.isLoading ? <tr><td colSpan={3}>(Loading...)</td></tr> :
+      Object.keys(get.data).length ?
+        Object.entries(get.data).map(([name, value]) =>
+          <tr key={name}>
+            <td>{
+              renamed && renamed.name == name ?
+                <input form={renameFormId} value={renamed.newName} onChange={e => { setRenamed({ ...renamed, newName: e.target.value }); }} /> :
+                name
+            }</td>
+            <td>{
+              edited && edited.name == name ?
+                <input form={updateFormId} value={edited.value} onChange={e => { setEdited({ ...edited, value: e.target.value }); }} /> :
+                renderValue ? renderValue(value) : value
+            }</td>
+            <td>
+              <button
+                onClick={async () => {
+                  try { await _delete.mutateAsync(name); }
+                  catch (e) { alert(e.response.data); }
+                }}
+                disabled={add.isLoading || _delete.isLoading || edited || renamed}
+              >
+                {_delete.isLoading && _delete.variables == name ? "Deleting..." : "❌"}
+              </button>
+              {
+                edited && edited.name == name && !update.isLoading ?
+                  <>
+                    <button form={updateFormId}>OK</button>
+                    <button form={updateFormId} onClick={() => { setEdited(undefined); }}>Cancel</button>
+                  </> :
+                  <button onClick={() => { setEdited({ name: name, value: value }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{
+                    update.isLoading && edited.name == name ? "Updating..." : "🖊"
+                  }</button>
+              }
+              {
+                renamed && renamed.name == name && !rename.isLoading ?
+                  <>
+                    <button form={renameFormId}>OK</button>
+                    <button form={renameFormId} onClick={() => { setRenamed(undefined); }}>Cancel</button>
+                  </> :
+                  <button onClick={() => { setRenamed({ name: name, newName: name }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{
+                    rename.isLoading && renamed.name == name ? "Renaming..." : "Rename"
+                  }</button>
+              }
+            </td>
+          </tr>
+        ) :
+        <tr><td colSpan={3}>(Nothing)</td></tr>
+    }
+    <tr>
+      <td><input form={addFormId} value={_new.name} onChange={e => { setNew({ ..._new, name: e.target.value }); }} /></td>
+      <td><input form={addFormId} value={_new.value} onChange={e => { setNew({ ..._new, value: e.target.value }); }} /></td>
+      <td><button form={addFormId} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{add.isLoading ? "Adding..." : "➕"}</button></td>
+    </tr>
+    <form id={addFormId} onSubmit={async e => {
+      e.preventDefault();
+      try {
+        await add.mutateAsync(_new);
+        setNew({ name: '', value: '' });
+      } catch (e) {
+        alert(e.response.data);
+      }
+    }} />
+    <form id={updateFormId} onSubmit={async e => {
+      e.preventDefault();
+      try {
+        await update.mutateAsync(edited);
+        setEdited(undefined);
+      } catch (e) {
+        alert(e.response.data);
+      }
+    }} />
+    <form id={renameFormId} onSubmit={async e => {
+      e.preventDefault();
+      try {
+        await rename.mutateAsync(renamed);
+        setRenamed(undefined);
+      } catch (e) {
+        alert(e.response.data);
+      }
+    }} />
+  </table>;
+}

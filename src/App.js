a9c62a3f895fedebe35863a0347d5dbaaf86732f
Jack Lu <jacklu@jacklu.me>
2023-07-19 20:22:15 +0800

    use antd

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import { Button, Input, Layout, theme } from 'antd';
 import Table from './Table';
 import './App.css';
 function App() {
   var queryClient = useQueryClient();
-  return <>
-    <h1>Chathub</h1>
-    <section>
-      <h2>app</h2>
-      <Table
-        get={useQuery(['app'], async () => (await axios.get("/app")).data)}
-        add={
-          useMutation({
-            mutationFn: newApp => axios.put(`/app/${newApp.name}`, JSON.stringify(newApp.value), { headers: { 'Content-Type': 'application/json' } }),
-            onSuccess: (data, newApp) => { queryClient.setQueryData(['app'], old => (old[newApp.name] = newApp.value, old)) }
-          })
-        }
-        _delete={
-          useMutation({
-            mutationFn: name => axios.delete(`/app/${name}`),
-            onSuccess: (data, name) => { queryClient.setQueryData(['app'], old => (delete old[name], old)) }
-          })
-        }
-        update={
-          useMutation({
-            mutationFn: editedApp => axios.put(`/app/${editedApp.name}`, JSON.stringify(editedApp.value), { headers: { 'Content-Type': 'application/json' } }),
-            onSuccess: (data, editedApp) => { queryClient.setQueryData(['app'], old => (old[editedApp.name] = editedApp.value, old)) }
-          })
-        }
-        rename={
-          useMutation({
-            mutationFn: renamedApp => axios(`/app/${renamedApp.name}`, { method: 'MOVE', headers: { 'Content-Type': 'application/json', 'Destination': `/app/${encodeURIComponent(renamedApp.newName)}`, 'Overwrite': 'F' } }),
-            onSuccess: (data, renamedApp) => { queryClient.setQueryData(['app'], old => { var oldValue = old[renamedApp.name]; delete old[renamedApp.name]; old[renamedApp.newName] = oldValue; return old; }); }
-          })
-        }
-        action={[
-          (value, name, { disabled }) => <button onClick={async () => {
-            var response = await axios.get(`/app/${name}/error`);
-            alert(
-              response.data
-                .map(error => `${new Date(error.time).toLocaleString()}\t${error.error}`)
-                .join('\n')
-            );
-          }} disabled={disabled}>Error log</button>
-        ]}
-        renderValue={value => <pre className="code-block"><code>{value}</code></pre>}
-        renderValueEdit={({ form, value, onChange, disabled }) => <textarea form={form} value={value} onChange={e => { onChange(e.target.value); }} disabled={disabled} />}
-      />
-    </section>
-  </>;
+  var { token } = theme.useToken();
+  return <Layout>
+    <Layout.Header style={{ backgroundColor: token.colorBgContainer }}>
+      <h1>Chathub</h1>
+    </Layout.Header>
+    <Layout.Content>
+      <section>
+        <h2>app</h2>
+        <Table
+          get={useQuery(['app'], async () => (await axios.get("/app")).data)}
+          add={
+            useMutation({
+              mutationFn: newApp => axios.put(`/app/${newApp.name}`, JSON.stringify(newApp.value), { headers: { 'Content-Type': 'application/json' } }),
+              onSuccess: (data, newApp) => { queryClient.setQueryData(['app'], old => (old[newApp.name] = newApp.value, old)) }
+            })
+          }
+          _delete={
+            useMutation({
+              mutationFn: name => axios.delete(`/app/${name}`),
+              onSuccess: (data, name) => { queryClient.setQueryData(['app'], old => (delete old[name], old)) }
+            })
+          }
+          update={
+            useMutation({
+              mutationFn: editedApp => axios.put(`/app/${editedApp.name}`, JSON.stringify(editedApp.value), { headers: { 'Content-Type': 'application/json' } }),
+              onSuccess: (data, editedApp) => { queryClient.setQueryData(['app'], old => (old[editedApp.name] = editedApp.value, old)) }
+            })
+          }
+          rename={
+            useMutation({
+              mutationFn: renamedApp => axios(`/app/${renamedApp.name}`, { method: 'MOVE', headers: { 'Content-Type': 'application/json', 'Destination': `/app/${encodeURIComponent(renamedApp.newName)}`, 'Overwrite': 'F' } }),
+              onSuccess: (data, renamedApp) => { queryClient.setQueryData(['app'], old => { var oldValue = old[renamedApp.name]; delete old[renamedApp.name]; old[renamedApp.newName] = oldValue; return old; }); }
+            })
+          }
+          action={[
+            (value, name, { disabled }) => <Button onClick={async () => {
+              var response = await axios.get(`/app/${name}/error`);
+              alert(
+                response.data
+                  .map(error => `${new Date(error.time).toLocaleString()}\t${error.error}`)
+                  .join('\n')
+              );
+            }} disabled={disabled}>Error log</Button>
+          ]}
+          renderValue={value => <pre className="code-block"><code>{value}</code></pre>}
+          renderValueEdit={({ form, value, onChange, disabled }) => <Input.TextArea form={form} value={value} onChange={e => { onChange(e.target.value); }} disabled={disabled} />}
+        />
+      </section>
+    </Layout.Content>
+  </Layout>;
 }
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,6 @@
 import { useState, useId } from 'react';
+import { Button, Input, Table as AntTable } from 'antd';
+import { PlusOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 export default function Table({
   get, add, _delete, update, rename, action,
   renderValue, renderValueEdit
@@ -9,68 +11,71 @@ export default function Table({
   var updateFormId = useId();
   var [renamed, setRenamed] = useState();
   var renameFormId = useId();
-  return <table>
-    {get.isLoading ? <tr><td colSpan={3}>(Loading...)</td></tr> :
-      Object.keys(get.data).length ?
-        Object.entries(get.data).map(([name, value]) =>
-          <tr key={name}>
-            <td>{
-              renamed && renamed.name == name ?
-                <input form={renameFormId} value={renamed.newName} onChange={e => { setRenamed({ ...renamed, newName: e.target.value }); }} disabled={rename.isLoading} /> :
-                name
-            }</td>
-            <td>{
-              edited && edited.name == name ?
-                renderValueEdit ?
-                  renderValueEdit({ form: updateFormId, value: edited.value, onChange: value => { setEdited({ ...edited, value: value }); }, disabled: update.isLoading }) :
-                  <input form={updateFormId} value={edited.value} onChange={e => { setEdited({ ...edited, value: e.target.value }); }} disabled={update.isLoading} /> :
-                renderValue ? renderValue(value) : value
-            }</td>
-            <td>
-              <button
-                onClick={async () => {
-                  try { await _delete.mutateAsync(name); }
-                  catch (e) { alert(e.response.data); }
-                }}
-                disabled={add.isLoading || _delete.isLoading || edited || renamed}
-              >
-                {_delete.isLoading && _delete.variables == name ? "Deleting..." : "❌"}
-              </button>
-              {
-                edited && edited.name == name && !update.isLoading ?
-                  <>
-                    <button form={updateFormId}>OK</button>
-                    <button form={updateFormId} onClick={() => { setEdited(undefined); }}>Cancel</button>
-                  </> :
-                  <button onClick={() => { setEdited({ name: name, value: value }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{
-                    update.isLoading && edited.name == name ? "Updating..." : "🖊"
-                  }</button>
-              }
-              {
-                renamed && renamed.name == name && !rename.isLoading ?
-                  <>
-                    <button form={renameFormId}>OK</button>
-                    <button form={renameFormId} onClick={() => { setRenamed(undefined); }}>Cancel</button>
-                  </> :
-                  <button onClick={() => { setRenamed({ name: name, newName: name }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{
-                    rename.isLoading && renamed.name == name ? "Renaming..." : "Rename"
-                  }</button>
-              }
-              {action.map(action => action(value, name, { disabled: add.isLoading || _delete.isLoading || edited || renamed }))}
-            </td>
-          </tr>
-        ) :
-        <tr><td colSpan={3}>(Nothing)</td></tr>
-    }
-    <tr>
-      <td><input form={addFormId} value={_new.name} onChange={e => { setNew({ ..._new, name: e.target.value }); }} disabled={add.isLoading} /></td>
-      <td>{
-        renderValueEdit ?
-          renderValueEdit({ form: addFormId, value: _new.value, onChange: value => { setNew({ ..._new, value: value }); }, disabled: add.isLoading }) :
-          <input form={addFormId} value={_new.value} onChange={e => { setNew({ ..._new, value: e.target.value }); }} disabled={add.isLoading} />
-      }</td>
-      <td><button form={addFormId} disabled={add.isLoading || _delete.isLoading || edited || renamed}>{add.isLoading ? "Adding..." : "➕"}</button></td>
-    </tr>
+  return <>
+    <AntTable
+      columns={[{
+        title: "Name",
+        dataIndex: 'name',
+        render: value => renamed && renamed.name == value ?
+          <Input form={renameFormId} value={renamed.newName} onChange={e => { setRenamed({ ...renamed, newName: e.target.value }); }} disabled={rename.isLoading} /> :
+          value
+      }, {
+        title: "Source",
+        dataIndex: 'value',
+        render: (value, { name }) => edited && edited.name == name ?
+          renderValueEdit ?
+            renderValueEdit({ form: updateFormId, value: edited.value, onChange: value => { setEdited({ ...edited, value: value }); }, disabled: update.isLoading }) :
+            <Input form={updateFormId} value={edited.value} onChange={e => { setEdited({ ...edited, value: e.target.value }); }} disabled={update.isLoading} /> :
+          renderValue ? renderValue(value) : value
+      }, {
+        title: "Action",
+        render: ({ name, value }) => <>
+          <Button
+            onClick={async () => {
+              try { await _delete.mutateAsync(name); }
+              catch (e) { alert(e.response.data); }
+            }}
+            disabled={add.isLoading || _delete.isLoading || edited || renamed}
+            loading={_delete.isLoading && _delete.variables == name}
+            danger
+            icon={<DeleteOutlined />}
+          />
+          {
+            edited && edited.name == name && !update.isLoading ?
+              <>
+                <Button form={updateFormId} htmlType={null}>OK</Button>
+                <Button form={updateFormId} onClick={() => { setEdited(undefined); }}>Cancel</Button>
+              </> :
+              <Button onClick={() => { setEdited({ name: name, value: value }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed} loading={update.isLoading && edited.name == name} icon={<EditOutlined />} />
+          }
+          {
+            renamed && renamed.name == name && !rename.isLoading ?
+              <>
+                <Button form={renameFormId} htmlType={null}>OK</Button>
+                <Button form={renameFormId} onClick={() => { setRenamed(undefined); }}>Cancel</Button>
+              </> :
+              <Button onClick={() => { setRenamed({ name: name, newName: name }); }} disabled={add.isLoading || _delete.isLoading || edited || renamed} loading={rename.isLoading && renamed.name == name}>Rename</Button>
+          }
+          {action.map(action => action(value, name, { disabled: add.isLoading || _delete.isLoading || edited || renamed }))}
+        </>
+      }]}
+      rowKey="name"
+      dataSource={get.data ? Object.entries(get.data).map(([name, value]) => ({ name, value })) : undefined}
+      loading={get.isLoading}
+      summary={data => <AntTable.Summary.Row>
+        <AntTable.Summary.Cell>
+          <Input form={addFormId} value={_new.name} onChange={e => { setNew({ ..._new, name: e.target.value }); }} disabled={add.isLoading} />
+        </AntTable.Summary.Cell>
+        <AntTable.Summary.Cell>{
+          renderValueEdit ?
+            renderValueEdit({ form: addFormId, value: _new.value, onChange: value => { setNew({ ..._new, value: value }); }, disabled: add.isLoading }) :
+            <Input form={addFormId} value={_new.value} onChange={e => { setNew({ ..._new, value: e.target.value }); }} disabled={add.isLoading} />
+        }</AntTable.Summary.Cell>
+        <AntTable.Summary.Cell>
+          <Button form={addFormId} htmlType={null} disabled={add.isLoading || _delete.isLoading || edited || renamed} loading={add.isLoading} icon={<PlusOutlined />} />
+        </AntTable.Summary.Cell>
+      </AntTable.Summary.Row>}
+    />
     <form id={addFormId} onSubmit={async e => {
       e.preventDefault();
       try {
@@ -98,5 +103,5 @@ export default function Table({
         alert(e.response.data);
       }
     }} />
-  </table>;
+  </>;
 }
